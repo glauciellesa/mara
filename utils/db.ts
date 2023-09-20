c8aa4a2e3ddcdf2d18d312e44b6acc62fa5abcdb
Glaucielle Sá <glauciellesa@gmail.com>
2023-09-20 22:12:52 +0200

    add db configuration

---
+++ utils/db.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const { MONGODB_URI } = process.env;
+
+const connection = {
+  isConnected: false,
+};
+
+const connect = async () => {
+  if (connection.isConnected) {
+    console.log("Already connected");
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected =
+      mongoose.connections[0].readyState ===
+      mongoose.ConnectionStates.connected;
+    if (connection.isConnected) {
+      console.log("Use previous connection");
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  if (MONGODB_URI) {
+    const db = await mongoose.connect(MONGODB_URI);
+    console.log("new connection");
+    connection.isConnected =
+      db.connections[0].readyState === mongoose.ConnectionStates.connected;
+  }
+};
+
+const disconect = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.disconnect();
+      connection.isConnected = false;
+    } else {
+      console.log("not disconnected");
+    }
+  }
+};
+
+const db = { connect, disconect };
+
+export default db;
