9892f4769cebb2860792fbccfdf9fc28bef82148
Glaucielle Sá <glauciellesa@gmail.com>
2023-09-21 22:07:33 +0200

    add preventDefault to button

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,35 +6,37 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { JWT } from "next-auth/jwt";
 import { Session } from "next-auth";
 
-/* interface UserForm {
+interface UserForm {
   _id: string;
   name: string;
   email: string;
   image: string;
   isAdmin: boolean;
-} */
+}
 
 export default NextAuth({
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    jwt: async (
-      { token, user } /* : {
+    jwt: async ({
+      token,
+      user,
+    }: {
       token: JWT;
       user: UserForm;
-    } */
-    ): Promise<JWT> => {
+    }): Promise<JWT> => {
       if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
       return token;
     },
-    session: async (
-      { session, token } /* : {
+    session: async ({
+      session,
+      token,
+    }: {
       session: Session;
       token: JWT;
-    } */
-    ): Promise<Session> => {
+    }): Promise<Session> => {
       if (token?._id) session.user._id = token._id;
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
       return session;
