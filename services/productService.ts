c13fab4ea9019b22db3d67837d292c35ae1d5308
Glaucielle Sá <glauciellesa@gmail.com>
2023-09-27 15:50:19 +0200

    fix product endpoint

--- services/productService.ts
+++ services/productService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const getProduct = async (id: string) => {
   console.log("to aqu");
 
-  const { data } = await axios.get(`api/products/${id}`);
+  const { data } = await axios.get(`/api/products/${id}`);
   return data;
 };
 
