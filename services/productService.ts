1c248fa29ec27a58ad0aa71da4b7c3ffd6790773
Glaucielle Sá <glauciellesa@gmail.com>
2023-09-27 15:48:32 +0200

    add product query

---
+++ services/productService.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const getProduct = async (id: string) => {
+  console.log("to aqu");
+
+  const { data } = await axios.get(`api/products/${id}`);
+  return data;
+};
+
+const service = { getProduct };
+
+export default service;
